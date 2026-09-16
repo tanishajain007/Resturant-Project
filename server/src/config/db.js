818bbf077adcd4cf47a799b02cb87dd444bbc2a2
tanishajain007 <taniahajain274@gmail.com>
2026-09-16 10:29:17 +0530

    New Change

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
+import env from "./env.js";
+
 const dbConnect = async () => {
-    await mongoose.connect("mongodb://localhost:27017/");
+    await mongoose.connect(env.DBURI);
     console.log("Connected DB Successfully");
     return;
 }
